d521edfad42beed41b39be0fd9ab42913336fc14
Murat Doganer <67741877+MuratDoganer@users.noreply.github.com>
2021-05-22 19:28:33 +0100

    Added LandTech Logo to the Intro Text

--- bot-response/blocks-intro.js
+++ bot-response/blocks-intro.js
@@ -7,6 +7,11 @@ const introBlocks = [
     "text": {
       "type": "mrkdwn",
       "text": ":wave: Hi there! I am your friendly *Rotation Bot* :arrows_counterclockwise: :robot_face: My job is to help the Land Tech team track different rotations, from who looks after Tech Support, to the editor of the _Engineering Weekly_. I am easy to use, and still in my early stages, so if you have any feedback, please let my creator, @<U010F3GJP5Y>, know"
+    },
+    "accessory": {
+      "type": "image",
+      "image_url": "https://landtech-it-bucket.s3.eu-west-2.amazonaws.com/512x512.png",
+      "alt_text": "LandTech Rota Bot"
     }
   },
   {
